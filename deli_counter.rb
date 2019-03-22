a918af97ac9df7ee321c3ba79b540835fd51a605
Bruce Tsvilikhovsky <bruce.tsvilikhovsky@flatironschool.com>
2019-03-22 20:19:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,15 @@
-def line(list, person){
-  list.each_index {| p |, index
-    
-  }
-}
+def line(line){
+  if line.empty?
+    puts "The line is currently empty."
+  else
+    output = "The line is currently:"
+    i = 0
+    while i < line.size
+      output += " #{i + 1}. #{line[n]}"
+      i += 1
+    end
+    puts output
+  end
+end
+
+def
