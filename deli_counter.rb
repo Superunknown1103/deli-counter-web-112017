65a95b6262cc8314bb1f433e7eebbaeedae7ffbd
Bruce Tsvilikhovsky <bruce.tsvilikhovsky@flatironschool.com>
2019-03-22 20:55:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(line)
   if line.empty?
     puts "The line is currently empty."
   else
-    output = "The line is currently:"
+    output = "The line is currently: "
     i = 0
     while i < line.size
       output += " #{i + 1}. #{line[n]}"
@@ -14,7 +14,7 @@ end
 
 def take_a_number(line, customer)
   line.push(customer)
-  return "Welcome, #{customer}. You are number #{line.size + 1}in line."
+  return "Welcome, #{customer}. You are number #{line.size + 1} in line."
 end
 
 def now_serving(line)
